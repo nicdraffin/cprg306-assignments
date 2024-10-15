0cc713b37c0bcfd6968ae578228f3984b5ab63d5
nicdraffin <nic.draffin@gmail.com>
2024-10-15 11:29:59 -0600

    Completed Assignment 5
    All Items for assignment have been completed and styling.

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Link href="./week-2/" className="underline text-cyan-600 hover:text-cyan-300">Week 2 Page</Link> <br></br>
       <Link href="./week-3/" className="underline text-cyan-600 hover:text-cyan-300">Week 3 Page</Link> <br></br>
       <Link href="./week-4/" className="underline text-cyan-600 hover:text-cyan-300">Week 4 Page</Link> <br></br>
-
+      <Link href="./week-5/" className="underline text-cyan-600 hover:text-cyan-300">Week 5 Page</Link> <br></br>
     </main>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function SimpleCounter() {
+
+  return (
+    <main className="p-7 align-middle backdrop-blur-2xl">
+        <h1 className="text-4xl text-red-600 mb-6">Week 4 Assignment</h1>
+        <NewItem />
+    </main>
+  );
+}
